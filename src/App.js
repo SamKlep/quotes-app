88fc4dc310b064cb4cc1b4cefe5f412103c7e2de
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-11 09:09:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
+import { Jumbotron } from "react-bootstrap";
 
 function App() {
+  const [responseData, setResponseData] = useState("");
+
+  useEffect(() => {
+    Axios({
+      method: "GET",
+      url: "https://quotes15.p.rapidapi.com/quotes/random/",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "quotes15.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+      },
+      params: {
+        language_code: "en",
+      },
+    })
+      .then((response) => {
+        setResponseData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [setResponseData, responseData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='title-container'>
+        <div className='title-border'>
+          <h1>Qutoes API</h1>
+        </div>
+      </div>
+      <br />
+      {responseData && (
+        <div className='container'>
+          <Jumbotron>
+            <blockquote>
+              "{responseData && responseData.content}"
+              <br />
+              <br />
+              <small>
+                {responseData &&
+                  responseData.originator &&
+                  responseData.originator.name}
+              </small>
+            </blockquote>
+          </Jumbotron>
+        </div>
+      )}
     </div>
   );
 }
